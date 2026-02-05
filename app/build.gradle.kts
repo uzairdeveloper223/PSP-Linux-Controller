5c90db9e2e2bf02ef5969acb94278b19e8c80f9f
Uzair Mughal <uzairmugakh90@gmail.com>
2026-02-05 14:00:02 +0500

    Release v1.0.3 - Add Qr Scanner and Game Stream

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "uzair.ppsspp.linuxcontroller"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
